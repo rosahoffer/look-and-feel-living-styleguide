94a202d3c0e61164cb8414017e49ba3af88d83c1
Tolga1999 <tolgadoganer@outlook.com>
2022-11-16 15:09:02 +0100

    small changes main

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-console.log("test")
+let link = document.querySelectorAll('a').forEach(link =>{
+    link.addEventListener('click', function(){
+        link.classList.add(".active")
+        console.log()
+    })
+})
