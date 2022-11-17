b08f42e1f4492d93ddd0c81ee2c1810237ccbd76
Tolga1999 <tolgadoganer@outlook.com>
2022-11-17 10:55:57 +0100

    active anchor elements
    probleem is opgelost

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
-let link = document.querySelectorAll('a').forEach(link =>{
-    link.addEventListener('click', function(){
-        link.classList.add(".active")
-        console.log()
+document.querySelectorAll('a').forEach(link => {
+    link.addEventListener('click', function () {
+        // checkt de ul voor active anchor elementen, 'this' is in dit geval de anchors
+        var active = this.parentNode.parentNode.querySelector('.active');
+        if (active) {
+            active.classList.remove('active');
+        }
+        this.classList.add('active')
     })
 })
